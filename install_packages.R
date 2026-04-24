22eed968e8b49ea811e29065da136573d9e52c48
jesus-baena <me@jbaena.net>
2026-04-24 11:16:46 +0300

    Gitignoring

--- install_packages.R
+++
@@ -1,5 +0,0 @@
-# Quick install script
-install.packages(c("dplyr", "ggplot2", "tidyr", "readr", "lubridate", "stringr"), 
-                 repos = "https://cloud.r-project.org/",
-                 dependencies = TRUE)
-cat("\n\n=== Installation Complete! ===\n")
--- install_quarto_packages.R
+++
@@ -1,5 +0,0 @@
-# Install packages needed for Quarto rendering
-install.packages(c("rmarkdown", "knitr"), 
-                 repos = "https://cloud.r-project.org/",
-                 dependencies = TRUE)
-cat("\n\n=== Quarto Packages Installed! ===\n")
